d6e18da6daafaa12e251e10cfe1f22b387f91662
Jonas <jonasmartinsrubim@gmail.com>
2022-01-18 16:14:19 -0300

    add novas nl

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -47,61 +47,61 @@ export default [
     description: "Falta iluminação/limpeza",
     reading: false,
   },
-   {
+  {
     id: 9,
     name: "1351",
     description: "Suspeita de medidor com ponteiros desregulados",
     reading: true,
   },
-   {
+  {
     id: 10,
     name: "1503",
     description: "Suspeita de irregularidade",
     reading: true,
   },
-   {
+  {
     id: 11,
     name: "1504",
     description: "Instalação ligada direta",
     reading: false,
   },
-   {
+  {
     id: 12,
     name: "1505",
     description: "Suspeita de medidor defeituoso/\navariado/manipulado/parado",
     reading: true,
   },
- {
+  {
     id: 13,
     name: "1506",
     description: "Medidor com disco girando ao contrario/desmarcando",
     reading: true,
   },
-   {
+  {
     id: 14,
     name: "1544",
     description: "Não identificado motivo do consumo zero",
     reading: true,
   },
-   {
+  {
     id: 15,
     name: "1546",
     description: "Consumo incompativel com imovel",
     reading: true,
   },
- {
+  {
     id: 16,
     name: "1562",
     description: "Instalação desligada sem consumo",
     reading: true,
   },
-   {
+  {
     id: 17,
     name: "3312",
     description: "Medidor com disco girando ao contrario/desmarcando",
     reading: false,
   },
-{
+  {
     id: 18,
     name: "3511",
     description: "Suspeita de medidor defeituoso/\navariado/manipulado/parado",
@@ -119,10 +119,17 @@ export default [
     description: "Mudança nome logradoro/nome faixada/razão social",
     reading: true,
   },
-    {
+  {
     id: 21,
     name: "1202",
     description: "Mudança numeraçao interna, apto ou vila de casas",
+    reading: true,
+  },
+  {
+    id: 22,
+    name: "2000",
+    description:
+      "Suspeita de medidor defeituoso em função das chuvas/enchentes",
     reading: true,
   },
   {
@@ -176,7 +183,8 @@ export default [
   {
     id: 31,
     name: "3205",
-    description: "Endereço de instalação não localizado/ alocado em local errado",
+    description:
+      "Endereço de instalação não localizado/ alocado em local errado",
     reading: false,
   },
   {
@@ -191,22 +199,23 @@ export default [
     description: "Suspeita de criadouro da DENGUE",
     reading: true,
   },
-{
+  {
     id: 34,
     name: "1801",
     description: "Instalação desabitada/Vazia",
     reading: true,
   },
- {
+  {
     id: 35,
     name: "1828",
     description: "Leitura conferida e confirmada",
     reading: true,
   },
- {
+  {
     id: 36,
     name: "1834",
-    description: "Leitura informada pelo cliente - NL PROIBIDA PARA LEITURISTA, FAVOR UTILIZAR 1898",
+    description:
+      "Leitura informada pelo cliente - NL PROIBIDA PARA LEITURISTA, FAVOR UTILIZAR 1898",
     reading: true,
   },
   {
@@ -215,132 +224,139 @@ export default [
     description: "Pontalete utilizada para outros fins",
     reading: true,
   },
-{
+  {
     id: 38,
     name: "1838",
     description: "Etiqueta descolando/ Sem etiqueta",
     reading: true,
   },
-{
+  {
     id: 39,
     name: "1841",
     description: "Instalação desativada sem medição",
     reading: false,
   },
-{
+  {
     id: 40,
     name: "1847",
     description: "Leitura informada formulário auto leitura rural",
     reading: true,
   },
-{
+  {
     id: 41,
     name: "1899",
     description: "Leitura confirmada em campo através de releitura",
     reading: false,
   },
-{
+  {
     id: 42,
     name: "1898",
     description: "Leitura informada pelo cliente formulário especifico",
     reading: true,
   },
-{
+  {
     id: 43,
     name: "5108",
     description: "Leitura impedida por cães/ Animais",
     reading: false,
   },
-{
+  {
     id: 44,
     name: "5802",
     description: "Instalação interditada",
     reading: true,
   },
-{
+  {
     id: 45,
     name: "6801",
     description: "Leitura impedida por condição insegura/ Área de risco",
     reading: false,
   },
-{
+  {
     id: 46,
     name: "6824",
-    description: "Calamidade não decretada/ Enchente alagamen/ Terrem/Vendaval-COVID-19",
+    description:
+      "Calamidade não decretada/ Enchente alagamen/ Terrem/Vendaval-COVID-19",
     reading: false,
   },
-{
+  {
     id: 47,
     name: "6836",
     description: "Obra pública sem acionamento concessionária",
     reading: false,
   },
-{
+  {
     id: 48,
     name: "6862",
     description: "Greve no serviço público",
     reading: false,
   },
-{
+  {
     id: 49,
     name: "6872",
     description: " Calamidade decretada/ Enchente alagamen/ Terrem/Vendaval ",
     reading: false,
   },
-{
+  {
     id: 50,
     name: "1901",
     description: "Caixa mal posicionada no poste",
     reading: true,
   },
-{
+  {
     id: 51,
     name: "1902",
     description: "Tampa do medidor danificado",
     reading: true,
   },
-{
+  {
     id: 52,
     name: "1903",
     description: "Chaminé defeituosa/ Medidor no poste",
     reading: true,
   },
-{
+  {
     id: 53,
     name: "1904",
     description: "Chapinha lidecore s/ Selo CPRED",
     reading: true,
   },
-{
+  {
     id: 54,
     name: "1905",
     description: "Sem etiqueta perigoso",
     reading: false,
   },
-{
+  {
     id: 55,
     name: "1906",
     description: "Prensa solta CPREDE",
     reading: true,
   },
-{
-    id: 54,
+  {
+    id: 56,
     name: "1907",
     description: "Necessita substituição",
     reading: true,
   },
-{
-    id: 55,
+  {
+    id: 57,
     name: "1978",
     description: "Medidor no poste com leitura",
     reading: true,
   },
 
   {
-    id: 55,
+    id: 58,
     name: "1101",
     description: "Leitura impedida por cães/ Animais",
     reading: true,
   },
-
-]
+  {
+    id: 60,
+    name: "7000",
+    description:
+      "Suspeita de medidor defeituoso em função das chuvas/enchentes",
+    reading: false,
+  },
+];
