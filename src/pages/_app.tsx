46cd3f2563b5239b1a9047051d6ab66c8dd58467
Jonas <jonasmartinsrubim@gmail.com>
2022-05-02 17:40:56 -0300

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,20 +7,25 @@ import Head from "next/head";
 import Footer from "ui/components/surfaces/Footer";
 import { AppContainer } from "@styles/pages/container";
 
+
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-144966040-2"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>NL-Leituritas</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"  />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap" rel="stylesheet" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
         <link rel="manifest" href="manifest.json" />
-      
+
       </Head>
       <ThemeProvider theme={theme}>
         <AppContainer>
