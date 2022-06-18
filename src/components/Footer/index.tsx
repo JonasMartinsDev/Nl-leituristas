fa5716714ef1aade0b125486dbf10a5fd6d4402a
jonasmartins <jonasmartinsrubim@gmail.com>
2022-06-18 11:27:49 -0300

    add correção :tada:

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           NL-Leiturista te ajuda a encontrar as Notas de leiturista perfeita
           para cada situação de sua tarefa.
         </p>
-        <a href="https://jonas.vercel.app/"> Developer by Jonas Martins </a>
+        <a href="https://jonas.vercel.app/" target={'_blank'}> Developer by Jonas Martins </a>
       </FooterContainer>
     </FooterStyled>
   );
